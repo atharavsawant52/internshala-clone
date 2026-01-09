b341a509507c38a531403f91e23874678dd4d96b
atharavsawant52 <atharavsawant52@gmail.com>
2026-01-09 23:31:55 +0530

    Fix missing forgot-password API route

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require("cors");
 const { connect } = require("./db");
 const router = require("./Routes/index");
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyparser.json({ limit: "50mb" }));
@@ -20,6 +20,10 @@ app.use((req, res, next) => {
   req.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Origin", "*");
   next();
+});
+
+app.use((req, res) => {
+  return res.status(404).json({ error: "Route not found" });
 });
 app.listen(port, () => {
   console.log(`Server is running on the port ${port}`);
